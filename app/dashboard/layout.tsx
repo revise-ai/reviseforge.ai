2e75927556afcf10373880d5057bff4c4a74ce89
Addo715 <emma141509@gmail.com>
2026-03-28 09:38:36 +0000

    update on contentpage

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,4 @@
 import Sidebar from "@/components/Sidebar";
-import { LanguageProvider } from "@/context/LanguageContext";
 
 export default function DashboardLayout({
   children,
@@ -7,11 +6,9 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <LanguageProvider>
-      <div className="flex">
-        <Sidebar />
-        <main className="ml-22.5 flex-1 min-h-screen">{children}</main>
-      </div>
-    </LanguageProvider>
+    <div className="flex">
+      <Sidebar />
+      <main className="ml-22.5 flex-1 min-h-screen">{children}</main>
+    </div>
   );
 }
